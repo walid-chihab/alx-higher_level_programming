28d4d3b791e9f91f40dda9c551c8388477d493b5
walid-chihab <walid-chihab@users.noreply.github.com>
2025-05-01 13:21:25 +0100

    simple

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,21 @@
+/****************************************************************************************/
+/* Write a script that prints 3 lines:							*/
+/* The first line: “C is fun”								*/
+/* The second line: “Python is cool”							*/
+/* The third line: “JavaScript is amazing”						*/
+/* You must use console.log(...) to print all output					*/
+/* You are not allowed to use var							*/
+/* guillaume@ubuntu:~/0x12$ ./1-multi_languages.js 					*/
+/* C is fun										*/
+/* Python is cool									*/
+/* JavaScript is amazing								*/
+/* guillaume@ubuntu:~/0x12$          							*/
+/****************************************************************************************/
+
+const var1 = "C is fun";
+const var2 = "Python is cool";
+const var3 = "JavaScript is amazing";
+
+console.log(var1)
+console.log(var2)
+console.log(var3)
