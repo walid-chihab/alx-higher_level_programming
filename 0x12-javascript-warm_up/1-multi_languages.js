3539937d0449dbb623459993d0a198a49d93d9ca
walid-walid <its.n.chihab@gmail.com>
2025-05-01 13:54:34 +0100

    Update 1-multi_languages.js
    The console.log() statements are missing semicolons at the end. While this will still work due to ASI, it's better to explicitly include semicolons for consistency and to prevent any unexpected behavior in more complex code.

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -16,6 +16,6 @@ const var1 = "C is fun";
 const var2 = "Python is cool";
 const var3 = "JavaScript is amazing";
 
-console.log(var1)
-console.log(var2)
-console.log(var3)
+console.log(var1);//more pro
+console.log(var2);
+console.log(var3);
