44464837897203c2987ae234269d65c04c47258c
walid-chihab <walid-chihab@users.noreply.github.com>
2025-05-08 00:38:43 +0100

    not completed

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,19 @@
+/**********************************************************************************************
+* Write a script that prints the first argument passed to it:
+ 
+*    If no arguments are passed to the script, print “No argument”
+*    You must use console.log(...) to print all output
+*    You are not allowed to use var
+*    You are not allowed to use length
+*   _______________________________________________________ 
+*  |guillaume@ubuntu:~/0x12$ ./3-value_argument.js 
+*  |No argument
+*  |guillaume@ubuntu:~/0x12$ ./3-value_argument.js School
+*  |School							
+*  |guillaume@ubuntu:~/0x12$ 					 |
+*  \_____________________________________________________________/
+*  Repo:
+* 	GitHub repository: alx-higher_level_programming
+* 	Directory: 0x12-javascript-warm_up
+* 	File: 3-value_argument.js
+***********************************************************************************************
